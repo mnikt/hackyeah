f9273111124dae969adc9bc248b991bf00fd0405
Marcin Gieroń <mgieron2012@gmail.com>
2024-10-06 16:46:28 +0200

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,19 +8,10 @@ from api.video_processor import VideoProcessor
 video_processor = VideoProcessor()
 
 
-def get_files_name(file) -> str:
-    print(type(file.file))
-
-    if hasattr(file.file, "name"):
-        return file.file.name
-    else:
-        return file.file.file.name
-
-
 @csrf_exempt
 def api(request: WSGIRequest) -> HttpResponse:
     file = request.FILES['file_0']
-    filename = get_files_name(file)
+    filename = file.file.file.name
     filesize = int(file.size / 1024 / 1024)
 
     data = video_processor.get_video_data(filename) | {
@@ -32,7 +23,7 @@ def api(request: WSGIRequest) -> HttpResponse:
 
 @csrf_exempt
 def comparison(request: WSGIRequest) -> HttpResponse:
-    filenames = [get_files_name(file) for file in request.FILES.values()]
+    filenames = [file.file.file.name for file in request.FILES.values()]
 
     data = {
         'comparison': video_processor.get_videos_comparison(filenames)
